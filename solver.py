418ab6beb95874f718f9691a2b2b4e7b72fd65fe
7usban0 <125022256+7usban0@users.noreply.github.com>
2024-07-05 19:47:43 +0300

    Pizza- SPCSCTF solver

--- solver.py
+++ solver.py
@@ -1,16 +1,20 @@
-def next_higher(n):
-    c=bin(n)[2:] #101
-    c=list(c)
-    m=""
-    for i in range(len(c)): #i=0
-        if(c[-1-i] =="1" and c[-2-i]=="0"):
-            c[-2-i]="1"
-            c[-1-i]="0"
-            for i in c:
-                m+=i
-            return m
-    return -1
+c = "9,4,6,8.6.2,9.2,;.2,9.9,2,2,7.3,4,7.2,2,9.4,7.3,3.7,9.1,9,4.0,9.8."
+n = ""
+result = ""
 
-
-print(next_higher(127))
-    
+for j in range(0, len(c), 2):
+    for i in range(97, 127):
+        if c[j] == str(chr(i // 2)):
+            if(c[j+1]=="." and  i%2==0):
+                result+=chr(i)
+                n+=chr(i//2)
+                n+="."
+            else:
+                result+=chr(i+1)
+                n+=chr((i+1)//2)
+                n+=","
+            break
+print(result, n)
+"""
+"simplereverseengineeringforcsharp" just change the b to a in before r    string
+"""
